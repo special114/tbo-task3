b32598284567e352006e79d8b746b4eee70f7e98
Rafał Surdej <special114@mailbox.org>
2023-12-11 22:01:28 +0100

    Fix JWT vulnerability

--- JWT/jwt-signature-apis-challenges/app.js
+++ JWT/jwt-signature-apis-challenges/app.js
@@ -28,8 +28,8 @@ app.post('/jwt/none', (req, res) => { //None endpoint
     const jwt_b64_dec = JWT.decode(jwt_token, { complete: true });
     if (jwt_b64_dec.header.alg == 'HS256') {
       secret_key = '885ae2060fbedcfb491c5e8aafc92cab5a8057b3d4c39655acce9d4f09280a20';
-    } else if (jwt_b64_dec.header.alg == 'none') {
-      secret_key = '';
+    } else {
+      res.status(400).send('Invalid JWT Token');
     }
     JWT.verify(jwt_token, secret_key, { algorithms: ['none', 'HS256'], complete: true, audience: 'https://127.0.0.1/jwt/none' }, (err, decoded_token) => {
       if (err) {
